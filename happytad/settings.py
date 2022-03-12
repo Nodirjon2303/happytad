a5ada7814d33098ce1b1e786d75b847fe5992274
Your Name <you@example.com>
2022-03-12 12:30:05 +0500

    nodir

--- happytad/settings.py
+++ happytad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bt9oi&d2-@5le!&fj)je$9jatv@yp!61k-5c*h$g-vprorl_uc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
